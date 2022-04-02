8a6c28f458ecc3ad3f5ab0cc36ad5896e33796ae
Day-101 <69306872+Day-101@users.noreply.github.com>
2022-04-02 15:20:42 +0200

    🍱  display port lauched

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello express!"));
+
+app.listen(port, () =>
+  console.log(
+    `Application lauched on : http://localhost:${port}`
+  )
+);
