eeeeb8e4cc687fba79234266a2ebb2689b85a200
Day-101 <69306872+Day-101@users.noreply.github.com>
2022-04-03 10:53:06 +0200

    ♻️ move code

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello express!"));
 
+app.get("/api/pokemons", (req, res) => {
+  const message = "La liste des pokemons a bien été récupérée."
+  res.json(success(message, pokemons));
+});
+
 app.get("/api/pokemons/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const pokemon = pokemons.find((pokemon) => pokemon.id === id);
   const message = "Un pokemon a bien été trouvé.";
   res.json(success(message, pokemon));
-});
-
-app.get("/api/pokemons", (req, res) => {
-  const message = "La liste des pokemons a bien été récupérée."
-  res.json(success(message, pokemons));
 });
 
 app.listen(port, () =>
