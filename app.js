3d2d7c3d4fff4f1cd92b10abdc30b4fa8658829b
Day-101 <69306872+Day-101@users.noreply.github.com>
2022-04-05 13:19:51 +0200

    ✨ add all pokemons in DB

--- app.js
+++ app.js
@@ -30,11 +30,18 @@ sequelize
 
 const Pokemon = PokemonModel(sequelize, DataTypes);
 
-sequelize
-  .sync({ force: true })
-  .then((_) =>
-    console.log('La base de données "Pokedex" a bien été synchronisée')
-  );
+sequelize.sync({ force: true }).then((_) => {
+  console.log('La base de données "Pokedex" a bien été synchronisée');
+  pokemons.map((pokemon) => {
+    Pokemon.create({
+      name: pokemon.name,
+      hp: pokemon.hp,
+      cp: pokemon.cp,
+      picture: pokemon.picture,
+      types: pokemon.types.join(),
+    }).then((pokemon) => console.log(pokemon.toJSON()));
+  });
+});
 
 app
   .use(favicon(__dirname + "/favicon.ico"))
