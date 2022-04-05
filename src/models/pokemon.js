e4c54e5b4fcb3cd375a35dc692129d8a18823bcc
Day-101 <69306872+Day-101@users.noreply.github.com>
2022-04-05 14:10:19 +0200

    ⚡️ add getter for types

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -26,6 +26,9 @@ module.exports = (sequelize, DataTypes) => {
       types: {
         type: DataTypes.STRING,
         allowNull: false,
+        get() {
+          return this.getDataValue("types").split(",");
+        }
       },
     },
     {
