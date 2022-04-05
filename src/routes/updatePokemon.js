a5597ee963908e036423b1dc6dc4104d8e98f60e
Day-101 <69306872+Day-101@users.noreply.github.com>
2022-04-05 17:39:03 +0200

    🐛 fix catch error

--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -1,12 +1,12 @@
 const { Pokemon } = require("../db/sequelize");
 
 module.exports = (app) => {
-  app
-    .put("/api/pokemons/:id", (req, res) => {
-      const id = req.params.id;
-      Pokemon.update(req.body, {
-        where: { id: id },
-      }).then((_) => {
+  app.put("/api/pokemons/:id", (req, res) => {
+    const id = req.params.id;
+    Pokemon.update(req.body, {
+      where: { id: id },
+    })
+      .then((_) => {
         return Pokemon.findByPk(id).then((pokemon) => {
           if (pokemon === null) {
             const message =
@@ -16,11 +16,11 @@ module.exports = (app) => {
           const message = `Le pokémon ${pokemon.name} a bien été modifié.`;
           res.json({ message, data: pokemon });
         });
+      })
+      .catch((error) => {
+        const message =
+          "Le pokemon n'a pas pu être modifié. Réssayez dans quelques instants.";
+        res.status(500).json({ message, data: error });
       });
-    })
-    .catch((error) => {
-      const message =
-        "Le pokemon n'a pas pu être modifié. Réssayez dans quelques instants.";
-      res.status(500).json({ message, data: error });
-    });
+  });
 };
