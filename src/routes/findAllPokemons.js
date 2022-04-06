ca6a1ba620c8058f342c534182596555b7aacee9
Day-101 <69306872+Day-101@users.noreply.github.com>
2022-04-06 12:33:05 +0200

    ⚡️ improve search func

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,10 +1,13 @@
 const { Pokemon } = require("../db/sequelize");
+const { Op } = require("sequelize");
 
 module.exports = (app) => {
   app.get("/api/pokemons", (req, res) => {
     if (req.query.name) {
       const name = req.query.name;
-      return Pokemon.findAll({ where: { name: name } }).then((pokemons) => {
+      return Pokemon.findAll({
+        where: { name: { [Op.like]: `%${name}%` } },
+      }).then((pokemons) => {
         const message = `Il y a ${pokemons.length} pokemons qui correspondent au terme de recherche ${name}.`;
         res.json({ message, data: pokemons });
       });
